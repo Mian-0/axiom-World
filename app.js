f6f7eb74b68863cc7896559c449693ddad20634f
Mian-0 <102787006+Mian-0@users.noreply.github.com>
2023-06-06 09:04:21 +0000

    half website done

--- app.js
+++ app.js
@@ -46,3 +46,40 @@ iconElement.addEventListener('click', function() {
     iconElement.classList.add('fa-bars');
   }
 });
+
+// SLider
+
+// CArds for services
+const boxes = document.getElementsByClassName('card');
+done
+
+for (let i = 0; i < boxes.length; i++) {
+  const box = boxes[i];
+
+  // Add a 'mouseover' event listener to each box
+  box.addEventListener('mouseover', function() {
+    // Add the 'highlight' class when the box is hovered
+    this.classList.add('hide-on-hover-services-section');
+  });
+
+  // Add a 'mouseout' event listener to each box
+  box.addEventListener('mouseout', function() {
+    // Remove the 'highlight' class when the mouse moves out of the box
+    this.classList.remove('highlight');
+  });
+}
+
+// Get the card and title elements
+var card = document.getElementById('card');
+var title = document.getElementById('spann');
+
+// Add event listener to the card element
+card.addEventListener('mouseenter', function() {
+  // Add the class to the title element
+  title.classList.add('hide-on-hover-services-section');
+});
+
+card.addEventListener('mouseleave', function() {
+  // Remove the class from the title element
+  title.classList.remove('hide-on-hover-services-section');
+});
