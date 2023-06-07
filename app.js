1a1b9164c85d4cdace46664c0fd89db4d3f98640
Mian-0 <102787006+Mian-0@users.noreply.github.com>
2023-06-07 11:34:58 +0000

    Consultation section Completed

--- app.js
+++ app.js
@@ -51,17 +51,34 @@ iconElement.addEventListener('click', function() {
 
 // CArds for services
 const boxes = document.getElementsByClassName('card');
-const titles = document.getElementsByClassName('Spann');
+const cardtitle = document.getElementsByClassName('custom-class-for-card-title');
+const cardtext = document.getElementsByClassName('card-text-hide');
+const cardtextonhover = document.getElementsByClassName('card-text');
+const cardicon = document.getElementsByClassName('fa-cloud');
+const cardbtn = document.getElementsByClassName('hide-card-btn');
 
 for (let i = 0; i < boxes.length; i++) {
   const box = boxes[i];
-  const title = titles[i];
+  const cardtexthide = cardtext[i];
+  const titles = cardtitle[i];
+  const color = cardtextonhover[i];
+  const icon = cardicon[i];
+  const btn = cardbtn[i];
 
   box.addEventListener('mouseover', function() {
-    title.classList.add('hide-on-hover-services-section');
+    titles.classList.add('show-on-hover-services-section');
+    cardtexthide.classList.add('hide-on-hover-services-section');
+    color.classList.add('card-text-on-hover');
+    icon.classList.add('fa-cloud-on-hover');
+    btn.classList.add('d-block');
   });
 
   box.addEventListener('mouseout', function() {
-    title.classList.remove('hide-on-hover-services-section');
+    titles.classList.remove('show-on-hover-services-section');
+    cardtexthide.classList.remove('hide-on-hover-services-section');
+    color.classList.remove('card-text-on-hover');
+    icon.classList.remove('fa-cloud-on-hover');
+    btn.classList.remove('d-none');
+
   });
 };
