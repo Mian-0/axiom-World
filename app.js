6dcd02060d4e1de8371d22a74e99f85f542151b4
Mian <muhammadbinakmal5@gmail.com>
2023-06-10 12:11:53 +0500

    garment page completed

--- app.js
+++ app.js
@@ -47,7 +47,6 @@ iconElement.addEventListener('click', function() {
   }
 });
 
-// SLider
 
 // CArds for services
 const boxes = document.getElementsByClassName('card');
@@ -81,4 +80,95 @@ for (let i = 0; i < boxes.length; i++) {
     btn.classList.remove('d-none');
 
   });
-};
+}
+//LOgos slider
+class LogoSlider {
+  constructor(elementId) {
+    this.slider = document.getElementById(elementId);
+    this.container = this.slider.querySelector('.logoContainer');
+    this.logos = this.container.querySelectorAll('.logo');
+    this.isDown = false;
+    this.startX = 0;
+    this.scrollLeft = 0;
+
+    this.attachEventListeners();
+    this.cloneLogos();
+  }
+
+  attachEventListeners() {
+    this.slider.addEventListener('mousedown', this.handleMouseDown.bind(this));
+    this.slider.addEventListener('mouseleave', this.handleMouseLeave.bind(this));
+    this.slider.addEventListener('mouseup', this.handleMouseUp.bind(this));
+    this.slider.addEventListener('mousemove', this.handleMouseMove.bind(this));
+
+    this.slider.addEventListener('touchstart', this.handleTouchStart.bind(this));
+    this.slider.addEventListener('touchend', this.handleTouchEnd.bind(this));
+    this.slider.addEventListener('touchmove', this.handleTouchMove.bind(this));
+
+    this.slider.addEventListener('scroll', this.handleScroll.bind(this));
+  }
+
+  cloneLogos() {
+    this.logos.forEach((logo) => {
+      const clonedLogo = logo.cloneNode(true);
+      this.container.appendChild(clonedLogo);
+    });
+  }
+
+  handleMouseDown(e) {
+    this.isDown = true;
+    this.startX = e.pageX - this.slider.offsetLeft;
+    this.scrollLeft = this.slider.scrollLeft;
+  }
+
+  handleMouseLeave() {
+    this.isDown = false;
+  }
+
+  handleMouseUp() {
+    this.isDown = false;
+  }
+
+  handleMouseMove(e) {
+    if (!this.isDown) return;
+    e.preventDefault();
+    const x = e.pageX - this.slider.offsetLeft;
+    const walk = x - this.startX;
+    this.slider.scrollLeft = this.scrollLeft - walk;
+  }
+
+  handleTouchStart(e) {
+    this.isDown = true;
+    this.startX = e.touches[0].clientX - this.slider.offsetLeft;
+    this.scrollLeft = this.slider.scrollLeft;
+  }
+
+  handleTouchEnd() {
+    this.isDown = false;
+  }
+
+  handleTouchMove(e) {
+    if (!this.isDown) return;
+    e.preventDefault();
+    const x = e.touches[0].clientX - this.slider.offsetLeft;
+    const walk = x - this.startX;
+    this.slider.scrollLeft = this.scrollLeft - walk;
+  }
+
+  handleScroll() {
+    const {
+      scrollLeft,
+      scrollWidth,
+      clientWidth
+    } = this.slider;
+    
+    if (scrollLeft + clientWidth >= scrollWidth) {
+      const clonedLogo = this.logos[0].cloneNode(true);
+      this.container.appendChild(clonedLogo);
+    }
+  }
+}
+
+// Usage
+const slider1 = new LogoSlider('slider1');
+// const slider2 = new LogoSlider('slider2');
